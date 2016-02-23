020916632a2fb7820e0c20c6c2143fbeab117c9e
ArturT <artur.trzop@lunarlogic.io>
2016-02-23 21:26:16 +0100

    We don’t need to specify again test http client to run tests for rumbl app. We need to run mix test from main directory of rumbrella (elixir umbrella app)

--- apps/rumbl/test/test_helper.exs
+++ apps/rumbl/test/test_helper.exs
@@ -1,12 +1,5 @@
-defmodule InfoSys.Test.HTTPClient do
-  def request(_url) do
-    {:ok, {[], [], "<queryresult></queryresult>"}}
-  end
-end
-
 ExUnit.start
 
 Mix.Task.run "ecto.create", ~w(-r Rumbl.Repo --quiet)
 Mix.Task.run "ecto.migrate", ~w(-r Rumbl.Repo --quiet)
 Ecto.Adapters.SQL.begin_test_transaction(Rumbl.Repo)
-
